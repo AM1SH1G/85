e91d62dfb86eb70c025ed69ce034c5dc72e2ac7d
AM1SH1G <109308281+AM1SH1G@users.noreply.github.com>
2022-11-06 10:57:26 +0000

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,19 @@
+menu_list_array = [ "Veg Margherita Pizza",                  
+"Garlic Dip","Veggie Deluxe", "Garlic Bread", "Dough Balls", "Pepperoni", "Hawaiian(ham & pineapple)"                                       ]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+	var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+}
+
